685eaf79c59e9208952e61d58936485ebdadedb3
changcheeqian <87271622+cheeqian@users.noreply.github.com>
2023-09-10 19:00:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cnjy01jqjjkq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "changcheeqian-employee"
+customregion = "us-east-1"
+
